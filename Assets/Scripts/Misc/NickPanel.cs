1eeefcda8a3052f61d35bc82e9501eae8b17b523
VladSharobaicko <61946717+VladSharobaicko@users.noreply.github.com>
2020-10-18 22:59:44 +0300

    added

---
+++ Assets/Scripts/Misc/NickPanel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Photon.Pun;
+using UnityEngine;
+
+namespace Misc
+{
+    public class NickPanel : MonoBehaviourPun
+    {
+        [SerializeField] private TextMesh text;
+
+        private void Start()
+        {
+            text.text = photonView.Owner.NickName;
+        }
+    }
+}
