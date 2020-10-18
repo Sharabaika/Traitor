1c8a3c2714a124ba73a876d7bc701d9e6269595d
VladSharobaicko <61946717+VladSharobaicko@users.noreply.github.com>
2020-10-18 22:56:57 +0300

    forgot to commit kek

---
+++ Assets/Scripts/MapObjects/InteractableObject.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Characters;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace MapObjects
+{
+    public class InteractableObject: MonoBehaviour
+    {
+        [SerializeField] private string interactionText;
+
+        [SerializeField] private UnityEvent<PlayerCharacter> OnUsedByCharacter;
+        [SerializeField] private bool isOutliningOnMouseOver = true;
+
+        [SerializeField] private Material outlineShader;
+        [SerializeField] private Material defaultShader;
+
+        private SpriteRenderer _renderer;
+        
+        private bool _isOutlining = false;
+        private bool IsOutlining
+        {
+            get => _isOutlining;
+            set
+            {
+                _isOutlining = value;
+                _renderer.sharedMaterial = value ? outlineShader : defaultShader;
+            }
+        }
+
+        private void Awake()
+        {
+            _renderer = GetComponent<SpriteRenderer>();
+        }
+
+        public virtual void Interact(PlayerCharacter with)
+        {
+            OnUsedByCharacter?.Invoke(with);
+        }
+
+        private void OnMouseEnter()
+        {
+            IsOutlining = true;
+        }
+
+        private void OnMouseExit()
+        {
+            IsOutlining = false;
+        }
+    }
+}
